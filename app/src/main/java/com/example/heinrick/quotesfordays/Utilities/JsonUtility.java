91a2c5823e7719a38f4276514a8dcf25e6562336
Heinrick <heinrick.lu@gmail.com>
2017-06-06 23:58:09 -0400

    Fixed Json Utility, it now works if a single quote is present the input String

--- app/src/main/java/com/example/heinrick/quotesfordays/Utilities/JsonUtility.java
+++ app/src/main/java/com/example/heinrick/quotesfordays/Utilities/JsonUtility.java
@@ -3,12 +3,14 @@ package com.example.heinrick.quotesfordays.Utilities;
 import com.example.heinrick.quotesfordays.models.Quote;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Heinrick on 2017-06-02.
@@ -23,28 +25,43 @@ public class JsonUtility {
     * @param    - String JsonInput: Json String
     * @return   - ArrayList<Quotes>*/
 
+
     public static ArrayList<Quote> formatJsonInput (String JsonInput){
 
-        ArrayList<Quote> quotes = new ArrayList<Quote>();
+        ArrayList<Quote> quoteList = new ArrayList<Quote>();
+
+
 
         try {
 
             JsonReader reader = new JsonReader(new StringReader(JsonInput));
             Gson gson = new GsonBuilder().create();
-            reader.beginArray();
+            //reader.beginArray();
 
-            while (reader.hasNext()) {
+            try {
+                Quote[] quotesArr = gson.fromJson(reader, Quote[].class);
+                quoteList = new ArrayList<Quote>(Arrays.asList(quotesArr));
+            }
+
+            catch (JsonSyntaxException e){
+                quoteList = new ArrayList < > ();
+                Quote quote = gson.fromJson(reader, Quote.class);
+                quoteList.add(quote);
+            }
+
+            /*while (reader.hasNext()) {
 
                 Quote quote = gson.fromJson(reader, Quote.class);
-                quotes.add(quote);
-            }
+                quoteList.add(quote);
+            }*/
             reader.close();
         }
 
         catch(IOException e){
             e.printStackTrace();
         }
-        return quotes;
+
+        return quoteList;
 
     }
 
